7bcce84a40306f8484a1d32db0e2b5c8179b50bf
jonschwadron <jonschwadron@gmail.com>
2015-10-09 10:41:58 -0400

    added localhost

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express     = require('express'),
     PORT        = process.env.PORT || 5432,
     server      = express(),
-    MONGOURI    = process.env.MONGOLAB_URI,
+    MONGOURI    = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
     dbname      = "db-cave",
     mongoose    = require('mongoose');
 
