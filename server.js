1f480992cdfe118e3604314f5c43034b73bac96a
jonschwadron <jonschwadron@gmail.com>
2015-10-09 10:39:58 -0400

    added mongodb

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express     = require('express'),
     PORT        = process.env.PORT || 5432,
-    server      = express();
+    server      = express(),
+    MONGOURI    = process.env.MONGOLAB_URI,
+    dbname      = "db-cave",
+    mongoose    = require('mongoose');
 
-server.get('/test', function (req, res) {
+server.get('/man-cave', function (req, res) {
   res.write("Welcome to my amazing app");
   res.end;
-
 });
+
+mongoose.connect(MONGOURI + "/" + dbname);
 
 server.listen(PORT, function() {
   console.log("SERVER IS UP ON PORT:", PORT);
